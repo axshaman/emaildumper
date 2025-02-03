a90bc146dd06f0b8e91d9b992708b279172f7ad6
axshaman <root@gcdev.info>
2025-02-03 11:03:52 +0100

    Ensuring all files are added

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "📦 Updating system and installing dependencies..."
-sudo apt update && sudo apt install -y python3 python3-pip python3-venv p7zip-full
+# sudo apt update && sudo apt install -y python3 python3-pip python3-venv p7zip-full
 
 echo "🚀 Setting up Python virtual environment..."
 python3 -m venv myenv
@@ -10,10 +10,7 @@ source myenv/bin/activate
 echo "📥 Installing Python dependencies..."
 pip install imapclient email-validator requests tqdm
 
-echo "📂 Cloning or downloading the script..."
-# If hosted on GitHub:
-git clone https://github.com/axshaman/emaildumper.git
-cd emaildumper
-
-echo "✅ Installation complete! Run the script with:"
-echo "   python dumper.py"
+echo "✅ Installation complete! Run the script with manual mode:"
+echo "python dumper.py --manual"
+echo "Run the script with automatical mode after edit config.ini"
+echo "nohup python dumper.py > log.txt 2>&1 &"
